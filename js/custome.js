65d616847613e64828b5a49a98914e9dcc0f62aa
AmanLama97 <lamaaman469@gamil.com>
2025-04-22 18:55:58 +0545

    commit

---
+++ js/custome.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $(".col-testimonal").owlCarousel({
+        loop: true,
+        items: 1,
+        margin: 20,
+        autoplay:true,
+        autoplay:true,
+        autoplayTimeout:4000,
+    });
+    $(".col-project").owlCarousel({
+        loop: true,
+        items: 1,
+        margin: 10,
+
+    });
+});
